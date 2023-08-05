d3f34efa7ea3d87f578630b71e2c335fa04ce53c
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-05 14:29:32 -0500

    configuracion boostrap y vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// Import our custom CSS
+import './App.css'
+import * as bootstrap from 'bootstrap'
+
+// Import all of Bootstrap's JS
+// import * as bootstrap from 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
