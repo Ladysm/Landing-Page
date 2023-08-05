fe2b303fa7cae1a174ab314ce53122416bed23a1
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-05 15:26:29 -0500

    fuentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import './App.css'
 import { Slide } from './components/Slide.jsx'
+import { Subscription } from './components/Subscription.jsx'
 // import { Carousel } from './img/img01.jpg'
 
 function App () {
@@ -9,6 +10,7 @@ function App () {
   return (
     <div className='App'>
       <Slide/>
+      <Subscription/>
 
     </div>
   )
