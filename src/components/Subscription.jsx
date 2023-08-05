f535cb0c1586c904c7f3bf000acb9148c1974a8d
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-05 16:47:26 -0500

    estilos button

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -10,8 +10,8 @@ export const Subscription = () => {
 
   return (
     <div className='square-subscription'>
-      <h2>¡Suscríbete y sé el primero en conocer las últimas tendencias en calzado deportivo!</h2>  
-      <button type="button" className="btn btn-secondary"onClick={handleClick}>Secondary</button>
+      <h2>¡Suscríbete y sé el primero en conocer las últimas tendencias en calzado deportivo!</h2>
+      <button type="button" className="btn btn-secondary btn-subscription"onClick={handleClick}>SUSCRÍBETE</button>
     </div>
   )
 }
