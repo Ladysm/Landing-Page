b4029746c983de6bfe5b3f2d35ad2753e5e5bf16
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-05 18:20:23 -0500

    error solucuonado

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,24 +1,65 @@
-import '../App.css';
-import { useState } from 'react';
-import { Modal } from 'react-modal';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
 export const Subscription = () => {
-  // Aquì utilizo el hook para inciarlizar el modal en false y asì no se muestra
-  // aqui hago el evento del boton
-  const [openModal, setOpenModal] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
   const handleClick = () => {
-    console.log('funciona el boton');
+    setIsModalOpen(true);
   };
 
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes realizar acciones con los datos del formulario, como enviarlos al servidor.
+    console.log('Nombre:', name);
+    console.log('Email:', email);
+    // Cerrar el modal después de enviar el formulario.
+    setIsModalOpen(false);
+  };
 
   return (
     <div className='square-subscription'>
       <h2>¡Suscríbete y sé el primero en conocer las últimas tendencias en calzado deportivo!</h2>
-      <button type="button" className="btn btn-secondary btn-subscription"onClick={handleClick}>SUSCRÍBETE</button>
-      <div className="modal-dialog modal-dialog-centered">
+      <button type="button" className="btn btn-secondary btn-subscription" onClick={handleClick}>
+        SUSCRÍBETE
+      </button>
 
-</div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={handleCloseModal}
+        contentLabel="Formulario de Suscripción"
+      >
+        <h2>Formulario de Suscripción</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Nombre:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Correo Electrónico:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
+      </Modal>
     </div>
   );
 };
