e6706ebdc6ebcc733e457ff7f251311a0bc823de
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-05 10:20:43 -0500

    imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../../../../../../vite.svg'
+// import { useState } from 'react'
+
 import './App.css'
+import { Carousel } from './components/carousel/carousel'
 
 function App () {
-  const [count, setCount] = useState(0)
+  /* const [count, setCount] = useState(0) */
 
   return (
-    <>
-
-    </>
+    <div className='App'>
+      <Carousel/>
+    </div>
   )
 }
 
