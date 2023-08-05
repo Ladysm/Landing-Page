8e3a10cdba17ba9df5ab5a6370b987b5d398bda8
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-04 22:29:57 -0500

    eslint okay

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
+
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'import/extensions': 0,
+    'no-var': 'error',
+    'prefer-const': 'error',
+    eqeqeq: 'error',
+    indent: ['error', 2]
+
+  }
+}
