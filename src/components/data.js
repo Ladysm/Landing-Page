a5e659a41e08b6ab94d06ed6652417d69b2d667f
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-06 16:59:35 -0500

    data organizado con links

--- src/components/data.js
+++ src/components/data.js
@@ -1,26 +1,29 @@
 export const data = {
   name: 'Nike Pegasus 40',
   slogan: 'Siéntelo para creerlo. Disponible en Nike Store.',
-  cities: [
+  links: [
     {
-      citiy1: 'BOGOTÁ' [
+      city: 'BOGOTÁ',
+      locations: [
         {
-          link1: 'https://www.google.com/maps/place/Nike+Store/@4.6668873,-74.0546586,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f9af5449e6bad:0xead6767acb510c3f!8m2!3d4.6668873!4d-74.0546586!16s%2Fg%2F1td77mk9?entry=ttu',
-          link2: 'https://www.google.com/maps/place/Nike+Store/@4.7322142,-74.0704009,17z/data=!4m7!3m6!1s0x8e3f85fe99686c6b:0xfe1c0be4b71a5f80!8m2!3d4.7322142!4d-74.066281!15sChtOaWtlIFN0b3JlIHBhcnVxZSBsYSBjb2xpbmFaHSIbbmlrZSBzdG9yZSBwYXJ1cWUgbGEgY29saW5hkgEUc3BvcnRpbmdfZ29vZHNfc3RvcmXgAQA!16s%2Fg%2F11dxn1ltws?coh=164777&entry=tt&shorturl=1',
-          link3: 'https://www.google.com/maps/place/Nike+Store+Gran+Estaci%C3%B3n/@4.6484197,-74.103695,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f9b112070e0fd:0x786bcacb8d9d1f70!8m2!3d4.6484144!4d-74.1015063!16s%2Fg%2F11h5l74lml?coh=164777&entry=tt&shorturl=1',
-          link4: 'https://www.google.com/maps/place/Nike+Store/@4.7624616,-74.0487249,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f8593ed8ebd33:0xb16af0207ce76c38!8m2!3d4.7624563!4d-74.0465362!16s%2Fg%2F1tg5d4gb?coh=164777&entry=tt&shorturl=1',
-          link5: 'https://www.google.com/maps/place/Nike+Store/@4.652524,-74.1296122,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f9d76d96dbd3b:0xf7dd79102bcbc157!8m2!3d4.6525187!4d-74.1274235!16s%2Fg%2F11f6561w1t?coh=164777&entry=tt&shorturl=1',
-          link6: 'https://www.google.com/maps/place/Nike+Store/@4.6316371,-74.1172675,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f8558ffceef4d:0xea708ea3a2510364!8m2!3d4.6316318!4d-74.1150788!16s%2Fg%2F11j3h48qrw?coh=164777&entry=tt&shorturl=1',
-          link7: 'https://www.google.com/maps/place/Nike+Store/@4.8834489,-74.0391732,17z/data=!3m1!4b1!4m6!3m5!1s0x8e40781f2ed00073:0x21fa4cb54de46331!8m2!3d4.8834436!4d-74.034946!16s%2Fg%2F11c2y70hxz?coh=164777&entry=tt&shorturl=1'
-        }
-      ],
-      city2: 'Medellìn'[
+          name: 'Nike Store Calle 82',
+          url: 'https://www.google.com/maps/place/Nike+Store/@4.6668873,-74.0546586,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f9af5449e6bad:0xead6767acb510c3f!8m2!3d4.6668873!4d-74.0546586!16s%2Fg%2F1td77mk9?entry=ttu'
+        },
         {
-          link6: 'https://www.google.com/maps/place/Nike+Store/@4.652524,-74.1296122,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f9d76d96dbd3b:0xf7dd79102bcbc157!8m2!3d4.6525187!4d-74.1274235!16s%2Fg%2F11f6561w1t?coh=164777&entry=tt&shorturl=1',
-          link7: 'https://www.google.com/maps/place/Nike+Store/@4.6316371,-74.1172675,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f8558ffceef4d:0xea708ea3a2510364!8m2!3d4.6316318!4d-74.1150788!16s%2Fg%2F11j3h48qrw?coh=164777&entry=tt&shorturl=1',
-          link8: 'https://www.google.com/maps/place/Nike+Store/@4.8834489,-74.0391732,17z/data=!3m1!4b1!4m6!3m5!1s0x8e40781f2ed00073:0x21fa4cb54de46331!8m2!3d4.8834436!4d-74.034946!16s%2Fg%2F11c2y70hxz?coh=164777&entry=tt&shorturl=1'
-
+          name: 'Nike Store Parque Colina',
+          url: 'https://www.google.com/maps/place/Nike+Store/@4.7322142,-74.0704009,17z/data=!4m7!3m6!1s0x8e3f85fe99686c6b:0xfe1c0be4b71a5f80!8m2!3d4.7322142!4d-74.066281!15sChtOaWtlIFN0b3JlIHBhcnVxZSBsYSBjb2xpbmFaHSIbbmlrZSBzdG9yZSBwYXJ1cWUgbGEgY29saW5hkgEUc3BvcnRpbmdfZ29vZHNfc3RvcmXgAQA!16s%2Fg%2F11dxn1ltws?coh=164777&entry=tt&shorturl=1'
         }
+        
+      ]
+    },
+    {
+      city: 'MEDELLIN',
+      locations: [
+        {
+          name: 'Nike Store Plaza Central',
+          url: 'https://www.google.com/maps/place/Nike+Store/@4.6316371,-74.1172675,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f8558ffceef4d:0xea708ea3a2510364!8m2!3d4.6316318!4d-74.1150788!16s%2Fg%2F11j3h48qrw?coh=164777&entry=tt&shorturl=1'
+        }
+       
       ]
     }
   ]
