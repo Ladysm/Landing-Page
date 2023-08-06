4dc6b14c4de31cbf9d2d58ef4b4ae267586fc2df
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-06 17:06:27 -0500

    image

--- src/data.js
+++ src/data.js
@@ -1,5 +1,8 @@
+import pegasusImage from './assets/img4.jgp';
+
 export const data = {
   name: 'Nike Pegasus 40',
+  img: './',
   slogan: 'Siéntelo para creerlo. Disponible en Nike Store.',
   links: [
     {
@@ -13,7 +16,7 @@ export const data = {
           name: 'Nike Store Parque Colina',
           url: 'https://www.google.com/maps/place/Nike+Store/@4.7322142,-74.0704009,17z/data=!4m7!3m6!1s0x8e3f85fe99686c6b:0xfe1c0be4b71a5f80!8m2!3d4.7322142!4d-74.066281!15sChtOaWtlIFN0b3JlIHBhcnVxZSBsYSBjb2xpbmFaHSIbbmlrZSBzdG9yZSBwYXJ1cWUgbGEgY29saW5hkgEUc3BvcnRpbmdfZ29vZHNfc3RvcmXgAQA!16s%2Fg%2F11dxn1ltws?coh=164777&entry=tt&shorturl=1'
         }
-        
+
       ]
     },
     {
@@ -23,7 +26,7 @@ export const data = {
           name: 'Nike Store Plaza Central',
           url: 'https://www.google.com/maps/place/Nike+Store/@4.6316371,-74.1172675,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f8558ffceef4d:0xea708ea3a2510364!8m2!3d4.6316318!4d-74.1150788!16s%2Fg%2F11j3h48qrw?coh=164777&entry=tt&shorturl=1'
         }
-       
+
       ]
     }
   ]
