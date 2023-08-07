40171468a2f5b8c4daf1820b9661ba3301ea5bec
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-06 20:24:28 -0500

    funciona

--- src/components/Linktreewerun/Linktreewerun.jsx
+++ src/components/Linktreewerun/Linktreewerun.jsx
@@ -9,7 +9,7 @@ import 'primeicons/primeicons.css';
 export const Linktreewerun = () => {
   return (
     <div className="square-linktree">
-      <div className="text-square-linktree"><h2>¡Visítanos y encuentra tu calzado deportivo ideal!</h2></div>
+      
       <div className='werun-container'>
 
         <Avatar image={data.img} className="mr-2" size="xlarge" shape="circle" />
--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,13 +1,19 @@
-import LinkTree from '../linktree/Linktree.jsx';
-import './linktree/linktree.css';
-import './components/Linktreewerun/Linktreewerun.css';
-import { Linktreewerun } from './Linktreewerun/Linktreewerun.jsx';
+import { Linktreewerun } from './Linktreewerun/Linktreewerun';
+import { Linktree } from '../linktree/Linktree';
+import './Container.css';
+import './Linktreewerun/Linktreewerun.css';
+import '../linktree/linktree.css';
+
 export const Container = () => {
   return (
     <div className='container-linktree'>
-
-      <LinkTree/>
+      <div className="text-square-container">
+        <h2>¡Visítanos y encuentra tu calzado deportivo ideal!</h2></div>
+      <div className='container-pegasus-werun'>
+      <Linktree/>
       <Linktreewerun/>
+      </div>
+      
 
     </div>
   );
