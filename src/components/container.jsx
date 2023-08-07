ca52614a238323674573b329788f56912948f5ae
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-07 09:40:02 -0500

    logica del linktree

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Linktreewerun } from './Linktreewerun/Linktreewerun';
 import { Linktree } from '../linktree/Linktree';
 import './Container.css';
@@ -5,16 +6,41 @@ import './Linktreewerun/Linktreewerun.css';
 import '../linktree/linktree.css';
 
 export const Container = () => {
+  // lo inicializo para que no se muestre
+  const [showLinktree, setShowLinktree] = useState(false);
+  const [showLinktreewerun, setShowLinktreewerun] = useState(false);
+  // creo el evento
+  const handleClickPegasus = () => {
+    setShowLinktree(!showLinktree);
+  };
+  const handleClickWeRun = () => {
+    setShowLinktreewerun(!showLinktreewerun);
+  };
+
   return (
     <div className='container-linktree'>
       <div className="text-square-container">
-        <h2>¡Visítanos y encuentra tu calzado deportivo ideal!</h2></div>
-      <div className='container-pegasus-werun'>
-      <Linktree/>
-      <Linktreewerun/>
+        <h2>¡Visítanos y encuentra tu calzado deportivo ideal!</h2>
       </div>
-      
-
+      <div className="button-container">
+        <button
+          type="button"
+          className="btn btn-warning button-pegaus"
+          onClick={handleClickPegasus}
+        >
+          Linktree pegasus
+        </button>
+        {showLinktree && <Linktree />}
+        <button
+          type="button"
+          className="btn btn-warning button-werun"
+          onClick={handleClickWeRun}
+        >
+          Linktree We Run
+        </button>
+        {showLinktreewerun && <Linktreewerun />}
+      </div>
     </div>
   );
 };
+
--- src/linktree/Linktree.jsx
+++ src/linktree/Linktree.jsx
@@ -10,16 +10,26 @@ export const Linktree = () => {
   return (
     <div>
       <div className='container-pegasus'>
-
         <Avatar image={data.img} className="image-pegasus" size="large" shape="circle" />
         <div className="container-subtitle"><h1 className="subtitle">{data.name}</h1></div>
-        <div className="slogan"><h3>{data.slogan}</h3> </div>
+        <div className="slogan"><h3>{data.slogan}</h3></div>
         <div className="container-buttons">
-        <button type="button" className="btn btn-warning button">Warning</button>
-
+          {data.links.find(city => city.city === 'BOGOTÁ').locations.map(location => (
+            <a href={location.url} target="_blank" rel="noopener noreferrer" key={location.name}>
+              <button type="button" className="btn btn-warning button">
+                {location.name}
+              </button>
+            </a>
+          ))}
+        </div>
       </div>
-      </div>
-          </div>
-
+    </div>
   );
 };
+
+
+
+
+
+
+
