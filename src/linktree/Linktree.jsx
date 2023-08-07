985dc22a28d2f627937c609bf9f3152101225a64
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-06 19:31:09 -0500

    error

--- src/linktree/Linktree.jsx
+++ src/linktree/Linktree.jsx
@@ -1,4 +1,4 @@
-import { data } from '../data';
+import { data, data2 } from '../data';
 import { Avatar } from 'primereact/avatar';
 import { Badge } from 'primereact/badge';
 import { useState } from 'react';
