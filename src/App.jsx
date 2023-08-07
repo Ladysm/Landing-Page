8020349105e91634688b446e2a5b309341d45302
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-06 19:03:17 -0500

    oriject we run empezando

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import { useState } from 'react'
 import './App.css';
 import './linktree/linktree.css';
+import './components/Linktreewerun/Linktreewerun.css';
 import { Slide } from './components/Slide.jsx';
 import { Subscription } from './components/Subscription.jsx';
 import { Linktree } from './linktree/Linktree';
+import {Linktreewerun } from './components/Linktreewerun/Linktreewerun.jsx';
 // import { Carousel } from './img/img01.jpg'
 
 function App () {
@@ -12,6 +14,8 @@ function App () {
       <Slide/>
       <Subscription/>
       <Linktree/>
+      <Linktreewerun/>
+      
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 import pegasusImage from './assets/img4.jpg';
+import weRunImage from './assets/img5.png';
 
 export const data = {
   name: 'Nike Pegasus 40',
@@ -31,3 +32,35 @@ export const data = {
     }
   ]
 };
+
+export const data2 = {
+  name: 'proyectowerun',
+  img: weRunImage,
+  slogan: 'Siéntelo para creerlo. Disponible en Nike Store.',
+  links: [
+    {
+      city: 'BOGOTÁ',
+      locations: [
+        {
+          name: 'Nike Store Calle 82',
+          url: 'https://www.google.com/maps/place/Nike+Store/@4.6668873,-74.0546586,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f9af5449e6bad:0xead6767acb510c3f!8m2!3d4.6668873!4d-74.0546586!16s%2Fg%2F1td77mk9?entry=ttu'
+        },
+        {
+          name: 'Nike Store Parque Colina',
+          url: 'https://www.google.com/maps/place/Nike+Store/@4.7322142,-74.0704009,17z/data=!4m7!3m6!1s0x8e3f85fe99686c6b:0xfe1c0be4b71a5f80!8m2!3d4.7322142!4d-74.066281!15sChtOaWtlIFN0b3JlIHBhcnVxZSBsYSBjb2xpbmFaHSIbbmlrZSBzdG9yZSBwYXJ1cWUgbGEgY29saW5hkgEUc3BvcnRpbmdfZ29vZHNfc3RvcmXgAQA!16s%2Fg%2F11dxn1ltws?coh=164777&entry=tt&shorturl=1'
+        }
+
+      ]
+    },
+    {
+      city: 'MEDELLIN',
+      locations: [
+        {
+          name: 'Nike Store Plaza Central',
+          url: 'https://www.google.com/maps/place/Nike+Store/@4.6316371,-74.1172675,17z/data=!3m1!4b1!4m6!3m5!1s0x8e3f8558ffceef4d:0xea708ea3a2510364!8m2!3d4.6316318!4d-74.1150788!16s%2Fg%2F11j3h48qrw?coh=164777&entry=tt&shorturl=1'
+        }
+
+      ]
+    }
+  ]
+}
