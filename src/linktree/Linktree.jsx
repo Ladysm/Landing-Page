6ec9dec153851a3b6f7bb82b938fe9b2151a1e84
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-06 22:05:04 -0500

    direccion organizada

--- src/linktree/Linktree.jsx
+++ src/linktree/Linktree.jsx
@@ -1,14 +1,14 @@
 import { data, data2 } from '../data';
 import { Avatar } from 'primereact/avatar';
 import { Badge } from 'primereact/badge';
-import { useState } from 'react';
+
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
 export const Linktree = () => {
   return (
-    <div >
+    <div className='container-pegasus' >
 
       <Avatar image={data.img} className="mr-2" size="xlarge" shape="circle" />
       <h1>{data.name}</h1>
