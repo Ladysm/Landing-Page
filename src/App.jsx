8c8dde7897e563d657bedea4c66276ae745e1a2f
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-07 21:57:37 -0500

    porfavor funciona

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 // import { useState } from 'react'
 import './App.css';
-import { Container } from './components/Container.jsx';
+import { Container } from './components/container.jsx';
 import { Slide } from './components/Slide.jsx';
 import { Subscription } from './components/Subscription.jsx';
 import { Youtube } from './components/youtube/Youtube';
 
-// import { Carousel } from './img/img01.jpg'
+// import { Carousel } from './img/img01.jpg k'
 
 function App () {
   return (
