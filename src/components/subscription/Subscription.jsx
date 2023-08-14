e7a93f19c8159eb70c3dd13db36b77a6cf123fe5
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-14 17:49:07 -0500

    organizando comentrios

--- src/components/subscription/Subscription.jsx
+++ src/components/subscription/Subscription.jsx
@@ -1,72 +1,84 @@
-import { useState, useEffect } from 'react';
+// importo los dos hooks
+import { useState } from 'react';
+// importo el modal de boostrap
 import Modal from 'react-modal';
+// importi la funciòn de firebase
 import { createpost } from '../../firebase/firebase';
 
+// creo un componente
 export const Subscription = () => {
+  // inicializo el modal en falso para que no se vea
   const [openModal, setOpenModal] = useState(false);
+  // incializo las variables como string
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  // incializo como false
   const [sent, setSent] = useState(false);
 
+  // creo un evento click en el boton subscrìbete
   const handleClick = () => {
+    // este me va cambiar el estado del modal a true para que sea visible
     setOpenModal(true);
   };
-
+  // esta funciòn es para que el usuario le de click por fuera del modal y se cierre
   const handleCloseModal = () => {
+    // se cambia el estado a false para que el modal se cierre
     setOpenModal(false);
   };
-  // hago esto para borrar el mensaje despuestde haberlo enviado
-
-  // Esto lo hago para borrar los campos del formulario cuando le den al boton enviar
+    // Est funciòn es para borrar los campos del formulario cuando le den al boton enviar
   const resetFields = () => {
+    // le cambio el esto a las variables como string vacio
     setName('');
     setEmail('');
-    setSent('');
+    setSent(false);
   };
+  // este es el evento submit para realizar el envìo de los datos capturados en los inputs
   const handleSubmit = (e) => {
+    // esto es para que la pagina no se recargue al darle click a enviar
     e.preventDefault();
-    // Aquí puedes realizar acciones con los datos del formulario, como enviarlos al servidor.
     console.log('Nombre:', name);
-    console.log('Email:', email);
+    // console.log('Email:', email);
     // aquì llamo a la funciòn  para guardar los datos en firestore
     createpost(name, email);
+    // aqui lo cambio a true porque bàsicamente me sirve para indicar que los datos han sido enviados correctamente
     setSent(true);
 
-    // Cerrar el modal después de 5 segundos
+    // Cerrar el modal después de 3 segundos
     setTimeout(() => {
+      // el modal se cierra
       setOpenModal(false);
+      // se resetean los campos al llamar la funciòn
       resetFields();
+      // se establece los milisegundos
     }, 3000);
   };
-  // Limpiar el estado de "sent" al abrir el modal nuevamente
-  useEffect(() => {
-    if (openModal) {
-      setSent(false);
-    }
-  }, [openModal]);
-
 
   return (
     <div className='square-subscription'>
       <div className='container-text'>
-      <h2>¡Suscríbete y sé el primero en conocer las últimas tendencias en calzado deportivo!</h2>
+        <h2>¡Suscríbete y sé el primero en conocer las últimas tendencias en calzado deportivo!</h2>
       </div>
       <button type="button" className="btn btn-secondary btn-subscription" onClick={handleClick}>
         SUSCRÍBETE
       </button>
-
+      {/* se muestra de acuerdo al valor true o false de la funciòn */ }
       <Modal
         isOpen={openModal}
         onRequestClose={handleCloseModal}
         contentLabel="Formulario de Suscripción"
         className={'custom-modal'}
       >
+        {/* se utiliza esa props para cerrar el modal cuando le doy click fuera del modal */ }
 
         <h3>Formulario de Suscripción</h3>
-
+        {/* se ejecuta la funciòn handleSubmit cuando le doy click a enviar */}
         <form className="form-modal" onSubmit={handleSubmit}>
           <div className="input-container" >
             <label htmlFor="name">Nombre:</label><br />
+            {/* capturo los datos */}
+            {/* uso el evente onChange que me permite identiifar cuando se cambia el contenido del input */}
+            {/* capturo el dato y lo seteo el estado de name */}
+
             <input
               type="text"
               id="name"
@@ -88,6 +100,7 @@ export const Subscription = () => {
           <div className="container-button-sent">
             <button type="submit">Enviar</button>
           </div>
+          {/* cuando sent sea verdadero se meustra el mensaje en un div, de lo contrario no se muestra */}
           {sent && <div className="mesaage-ok"> Datos enviados correctamente <ion-icon name="chevron-down-circle"></ion-icon></div>}
 
         </form>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 // Importo firebase
-
 import { initializeApp } from 'firebase/app';
+// importo los metodos que voy a utilizar
+// addDoc para añadir documentos
+// firestore para usar la bd
+// servertime para
 import { getFirestore, collection, addDoc, serverTimestamp } from 'firebase/firestore';
-// o los otros módulos que necesites, como auth, storage, etc.
 
-// y la BD de datos nosql firestore
-// importo el mètodo desde firestore para usarlo
-
+// pego esta conifguraciòn
 const firebaseConfig = {
   apiKey: 'AIzaSyDke0USk-R53TC9S1ksxif0-x3puoxnFRk',
   authDomain: 'landing-page-77329.firebaseapp.com',
@@ -21,21 +21,16 @@ export const app = initializeApp(firebaseConfig);
 // incializo firestore BD
 const db = getFirestore(app);
 
-// Aqui utilizo creo una funciòn para utilizar
-/*
-export const createpost = (name, email) => addDoc((db, 'post'), {
-name,
-email,
-createdAt: serverTimestamp(Date)
-  
-});
-*/
+// creo la funciòn que recibe los parametros nombre y correo
 export const createpost = (name, email) => {
-  addDoc(collection(db, 'post'), {
+  // utilizo la funciòn addDoc, para añadir un documento a la colecciòn la cual se llama data
+  // en el objeto esta lo que se va guardar en el db, es decir nombre, email y fecha de creaciòn
+  addDoc(collection(db, 'data'), {
     name,
     email,
     createdAt: serverTimestamp()
   })
+  // utilizo una promesa para verificar que si funcione correctamente
     .then(() => {
       console.log('funciona!!!');
     })
@@ -43,6 +38,3 @@ export const createpost = (name, email) => {
       console.error('error:', error);
     });
 };
-
-
-// export const colRef = collection(db, 'post');
