32eb5156195f9ad3da4f0cd2c6caea7feaa88d83
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-08-14 10:57:20 -0500

    comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-// import { useState } from 'react'
-import './App.css';
 import { Container } from './components/container/container.jsx';
 import { Slide } from './components/Slide.jsx';
 import { Subscription } from './components/Subscription.jsx';
 import { Youtube } from './components/youtube/Youtube';
-
-// import { Carousel } from './img/img01.jpg k'
-
+// Compinente que organiza y renderiza los demàs sub componentes
 function App () {
   return (
     <div className='App'>
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 /* realizo las correspondientes importaciones */
-// para usar react, el modal e importar el archivo app
+// para usar react
 import React from 'react';
+// Importa ReactDOM, que es la biblioteca utilizada para renderizar los componentes
 import ReactDOM from 'react-dom/client';
+// Importa el componente principal de la aplicación desde el archivo App.jsx
 import App from './App.jsx';
+// importa el modal
 import Modal from 'react-modal';
-// Import our custom CSS
+// Importa los estilos
 import './App.css';
-
+// configuro el modal para vincularlo a la raiz de mi aplicaciòn para que funcione bien
 Modal.setAppElement('#root');
-
+//  aquì estoy redenderizando en mi elemento root que se encuentra en el html
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // permite identificar problemas que pueda afectar el renderizado
   <React.StrictMode>
     <App />
   </React.StrictMode>
